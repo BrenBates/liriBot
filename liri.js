69baec45c6e754cf14f8704a488d54c7dff71e96
Bren <brennenbates2859@gmail.com>
2019-10-17 20:29:31 -0600

    Added funcitons to Liri bot
    Added operations for 'movie-this', 'concert-this' and 'spotify-this-song' to Liri bot.

--- liri.js
+++ liri.js
@@ -1,23 +1,27 @@
+////////////////////////////////
+// Variables and Requirements //
+////////////////////////////////
+
 require("dotenv").config();
+var axios = require("axios");
+var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+var moment = require('moment');
+moment().format();
+let operation = process.argv[2];
+let input = process.argv[3];
+
 
 /////////////////////
 // Spotify Content //
 /////////////////////
 
-var Spotify = require('node-spotify-api');
-
-var keys = require("./keys.js");
-
-var spotify = new Spotify(keys.spotify);
-
-let operation = process.argv[2];
-let input = process.argv[3];
-
-if(!input){
-    input = 'The Sign Ace of Base';
-}
-
 if(operation === 'spotify-this-song') {
+
+    if(!input){
+        input = 'The Sign Ace of Base';
+    }
 
     spotify.search({ type: 'track', query: input, limit: 1 }, function(err, data) {
         if (err) {
@@ -44,6 +48,71 @@ if(operation === 'spotify-this-song') {
        console.log('Preview URL: ' + Data.preview_url);
       
        
-    
 
 })};
+
+//////////////////////////
+// Movie-this: OMDB API //
+//////////////////////////
+
+if(operation === 'movie-this'){
+    if(!input){
+        input = 'Mr. Nobody';
+    }
+
+    axios.get('http://www.omdbapi.com/?apikey=trilogy&limit=1&t='+input).then(
+        function(response) {
+
+            let Data = response.data;
+            let rottenScore = '';
+
+            console.log('Title: ' + Data.Title);
+            console.log('Release Year: ' + Data.Year);
+            console.log('IMDB Rating: ' + Data.imdbRating);
+
+            for(let i =0; i < Data.Ratings.length; i++) {
+                if(Data.Ratings[i].Source === 'Rotten Tomatoes'){
+                     rottenScore = Data.Ratings[i].Value;
+                    
+                }
+            } 
+
+            console.log('Rotten Tomatoes Rating: ' + rottenScore );
+            console.log('Country Where Movie Was Produced: ' + Data.Country);
+            console.log('Language of the movie: ' + Data.Language);
+            console.log('Movie Plot: ' + Data.Plot);
+            console.log('Actors: ' + Data.Actors);
+           
+
+        
+            
+        }
+    )
+
+};
+
+/////////////////////////////////////
+// Concert-this: Bands in Town API //
+/////////////////////////////////////
+
+if(operation === 'concert-this'){
+    axios.get('https://rest.bandsintown.com/artists/'+input+'/events?app_id=codingbootcamp').then(
+        function(response) { 
+            let Data = response.data[0];
+
+            console.log('Venue Name: ' + Data.venue.name);
+            console.log('Venue Location ' +Data.venue.city + ' ' + Data.venue.region + ' ' + Data.venue.country); 
+
+            let eventDate = Data.datetime;
+            eventDate = moment().utc().format('MM/DD/YYYY');
+            console.log('Event Date: ' + eventDate);
+        }
+    )};
+
+///////////////////// 
+// do-what-it-says //
+/////////////////////
+
+if(operation === 'do-what-it-says') {
+    
+}
